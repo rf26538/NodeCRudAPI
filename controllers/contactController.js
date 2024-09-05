3fd00a1c7b83a0e20d5c0310d4bb63c893a1d3ed
manisht4 <manisht4@chetu.com>
2024-09-05 23:16:58 +0530

    Use Async express handler in controllers

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,8 +1,9 @@
+const asyncHandler = require("express-async-handler");
 const getContacts = (req, res) => {
     res.status(200).json({message :"Get All contact"});
 };
 
-const createContact = (req, res) => {
+const createContact = asyncHandler((req, res) => {
     const {name, age, email, phone } = req.body;
 
     if (!name || !email || !phone) {
@@ -10,19 +11,19 @@ const createContact = (req, res) => {
         throw new Error("All fields are mendatory");
     }
     res.status(200).json({message :"create contact"});
-};
+});
 
-const updateContact =(req, res) => {
+const updateContact = asyncHandler((req, res) => {
     res.status(200).json({message :`update contact for ${req.params.id}`});
-};
+});
 
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler((req, res) => {
     res.status(200).json({message :`Delete contact for ${req.params.id}`});
-};
+});
 
-const getIndivisualContact = (req, res) => {
+const getIndivisualContact = asyncHandler((req, res) => {
     res.status(200).json({message :`Indivisual contact for ${req.params.id}`});
-};
+});
 
 
 module.exports = { 
