e75fa965be6740da7c472a19908862667b2aa742
manisht4 <manisht4@chetu.com>
2024-09-05 23:13:19 +0530

    Define and use errros

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,27 +1,45 @@
+const { constants } = require("../constants");
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
     switch (statusCode) {
-        case 400:
+        case constants.NOT_FOUND:
             res.json({
                 title: "Not Found",
                 message : err.message,
                 stackTrace: err.stack
             });
             break;
-
-        case 404:
+        case  constants.VALIDATION_ERROR:
             res.json({
                     title: "Validation Failed",
                     message : err.message,
                     stackTrace: err.stack
                 });
             break;
-    
-        default:
+        case constants.UNAUTHORIZE:
             res.json({
+                title: "Unauthorize",
                 message : err.message,
                 stackTrace: err.stack
             });
+            break;
+        case  constants.FORBIDDEN:
+            res.json({
+                    title: "Forbidden!",
+                    message : err.message,
+                    stackTrace: err.stack
+                });
+            break;
+        case  constants.SERVER_ERROR:
+            res.json({
+                    title: "Server error",
+                    message : err.message,
+                    stackTrace: err.stack
+                });
+            break;
+
+        default:
+            console.log("No error all good");
             break;
     }
 }
