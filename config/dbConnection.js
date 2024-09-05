f07f17fb2a8354c5e0b16a7555d29b794962d1b0
manisht4 <manisht4@chetu.com>
2024-09-05 23:50:50 +0530

    Connected to db

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongooes");
+const mongoose = require("mongoose");
 
 const connectDb = async () => {
     try {
-        const connect = await mongoose.connect(process.env.CONNECTION_STING);
-        console.log("Database conencted:", connect.connection.host);
+        const connect = await mongoose.connect("mongodb+srv://rf26538:rf26538@cluster0.ig1phbq.mongodb.net/mycontact-backend?retryWrites=true&w=majority&appName=Cluster0");
+        console.log("Database conencted:", connect.connection.host, connect.connection.name);
     } catch (err) {
         console.log(err);
         process.exit(1);
